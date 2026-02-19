35a8be340550acf69e7ddb6316674777a7538f6a
DaraghWallace <daraghwallace99@gmail.com>
2026-02-19 14:14:38 +0800

    $ cdk init app --language typescript

---
+++ tracker-cdk/test/tracker-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TrackerCdk from '../lib/tracker-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tracker-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TrackerCdk.TrackerCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
